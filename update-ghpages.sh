1f4a051753d3f15fe01e1246cf86fbfc8582495e
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2016-06-16 11:00:31 -0700

    push.

--- update-ghpages.sh
+++ update-ghpages.sh
@@ -2,4 +2,4 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 git remote add writable https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null
 git remote show
-ghp-import -r writable output/
+ghp-import -p -r writable output/
