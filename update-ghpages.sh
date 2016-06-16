8d48c1d25b915bd51622a5bfb77a60b232da8839
Travis <travis@travis-ci.org>
2016-06-16 11:38:30 -0700

    avoid messages.

--- update-ghpages.sh
+++ update-ghpages.sh
@@ -1,10 +1,13 @@
+(
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
+
 git remote add writable https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null
 git fetch writable
 git fetch origin
 
 cd output/
-ghp-import -p -m "Travis auto commit" -r writable . > /dev/null
+ghp-import -p -m "Travis auto commit" -r writable .
+) 1>/dev/null 2> /dev/null
 
 echo Deployed to http://`dirname ${TRAVIS_REPO_SLUG}`.github.io/`basename ${TRAVIS_REPO_SLUG}`
