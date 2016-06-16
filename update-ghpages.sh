43746744caeac19419614bed7be8bfe236e86208
Travis <travis@travis-ci.org>
2016-06-16 11:35:08 -0700

    new token and fetch before ghp-import

--- update-ghpages.sh
+++ update-ghpages.sh
@@ -1,8 +1,10 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 git remote add writable https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null
+git fetch writable
+git fetch origin
 
 cd output/
-ghp-import -p -m "Travis auto commit" -r writable .
+ghp-import -p -m "Travis auto commit" -r writable . > /dev/null
 
 echo Deployed to http://`dirname ${TRAVIS_REPO_SLUG}`.github.io/`basename ${TRAVIS_REPO_SLUG}`
